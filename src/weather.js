4f0f6265980bda18a32d029760807858abe624f8
Shrishti <shrishtic9@gmail.com>
2022-05-28 21:18:32 +0530

    Weather app in react

---
+++ src/weather.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from 'react';
+import './weather.css'
+
+const api = {
+    key: "7358b1a11c438706329073b766ac9fc5",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const Weather = () => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    const [location, setLocation] = useState("");
+    const [liveweather, setLiveweather] = useState({});
+
+    function changer(e) {
+        setLocation(e.target.value);
+    }
+
+    const search = (e) => {
+        if (e.key == "Enter") {
+            fetch(`${api.base}weather?q=${location}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    setLiveweather(result);
+                    setLocation("");
+                    console.log(result);
+                });
+        }
+
+    }
+
+    console.log(liveweather)
+    today = mm + '/' + dd + '/' + yyyy;
+
+    return (
+        <div className='weather'>
+            <div className='header'>
+                <input type="text" placeholder='Enter location here' name="location" onChange={changer} onKeyPress={search} />
+                <div className='date'>
+                    {`${today}`}
+                </div>
+                <div className='weather__update'>
+                    <h1>Current Temp : {liveweather.main.temp}</h1>
+                    <p>Weather :{liveweather.weather[0].main} </p>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Weather;
